11f1962be7b24b55c826e208a3c08a94ebc251f8
Adarsh <adarshannaiynar.sivashunmugam@mrcooper.com>
2017-09-01 21:15:41 +0530

    Added extensive tests

--- src/client/actions/actionCreators.js
+++ src/client/actions/actionCreators.js
@@ -8,7 +8,17 @@ const decrementAction = () => ({
   type: actions.DECREMENT,
 });
 
+const dispatchIncrement = () => {
+  return dispatch => dispatch(incrementAction());
+};
+
+const dispatchDecrement = () => {
+  return dispatch => dispatch(decrementAction());
+};
+
 export default {
   incrementAction,
   decrementAction,
+  dispatchIncrement,
+  dispatchDecrement,
 };
--- test/unit/components/App.test.jsx
+++ test/unit/components/App.test.jsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { expect } from 'chai';
-import App from '../../../src/client/components/App';
+import App from 'components/App';
 
 const initiator = () => {
   const props = {
@@ -22,6 +22,19 @@ describe('Testing App Component', () => {
 
     it('must render component', () => {
       expect(wrapper.exists()).to.be.true;
+    });
+
+    it('Must render the p element', () => {
+      expect(wrapper.find('p').exists()).to.be.true;
+      expect(wrapper.find('p').text()).to.be.eql('Value: 1');
+    });
+
+    it('Must render the increment button', () => {
+      expect(wrapper.find('#increment_button').exists()).to.be.true;
+    });
+
+    it('Must render the decrement button', () => {
+      expect(wrapper.find('#decrement_button').exists()).to.be.true;
     });
   });
 });
